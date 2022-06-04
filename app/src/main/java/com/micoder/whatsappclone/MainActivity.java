589039f0aa96544e6dd68c64c6eb614acd0e9624
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-06-05 00:05:59 +0530

    tablayout added with fragments

--- app/src/main/java/com/micoder/whatsappclone/MainActivity.java
+++ app/src/main/java/com/micoder/whatsappclone/MainActivity.java
@@ -2,12 +2,18 @@ package com.micoder.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolBar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdpater myTabsAccessorAdpater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         mToolBar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolBar);
         getSupportActionBar().setTitle("WhatsApp");
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdpater = new TabsAccessorAdpater(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdpater);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
 
     }
 }
