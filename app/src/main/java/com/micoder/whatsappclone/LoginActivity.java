31df849489ad1e81e0d31bc4a5592d60a3a5bd14
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-06-07 14:12:29 +0530

    login register activity text link added

--- app/src/main/java/com/micoder/whatsappclone/LoginActivity.java
+++ app/src/main/java/com/micoder/whatsappclone/LoginActivity.java
@@ -4,24 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEmail, UserPassword;
+    private TextView NeedNewAccountLink, ForgetPasswordLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields();
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void InitializeFields() {
+        LoginButton = (Button) findViewById(R.id.login_button);
+        PhoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        ForgetPasswordLink = (TextView) findViewById(R.id.forget_password_link);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        if (currentUser == null){
+        if (currentUser != null){
             SendUserToMainActivity();
         }
     }
@@ -29,6 +54,10 @@ public class LoginActivity extends AppCompatActivity {
     private void SendUserToMainActivity() {
         Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(mainIntent);
+    }
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 
 }
