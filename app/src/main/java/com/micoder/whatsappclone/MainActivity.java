0c1e8efd7bd9db9e7e24fbd7a105abd2329160ad
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-06-07 22:28:04 +0530

    option menu added with logout feature

--- app/src/main/java/com/micoder/whatsappclone/MainActivity.java
+++ app/src/main/java/com/micoder/whatsappclone/MainActivity.java
@@ -1,11 +1,14 @@
 package com.micoder.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -54,5 +57,33 @@ public class MainActivity extends AppCompatActivity {
     private void SendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.main_logout_option){
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        if (item.getItemId() == R.id.main_settings_option){
+
+        }
+        if (item.getItemId() == R.id.main_find_friends_option){
+
+        }
+
+        return true;
     }
 }
