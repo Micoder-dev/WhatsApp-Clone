d1ff21e9e102a82630edf41daf19c23135087b79
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-06-09 15:39:24 +0530

    groupchat activity added with message design

---
+++ app/src/main/java/com/micoder/whatsappclone/GroupChatActivity.java
@@ -0,0 +1,39 @@
+package com.micoder.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton SendMessageButton;
+    private EditText userMessageinput;
+    private ScrollView mScrollView;
+    private TextView displayTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+
+        InitializeFields();
+
+    }
+
+    private void InitializeFields() {
+        mToolbar = (Toolbar) findViewById(R.id.group_chat_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Group Name");
+
+        SendMessageButton = (ImageButton) findViewById(R.id.send_message_button);
+        userMessageinput = (EditText) findViewById(R.id.input_group_message);
+        displayTextMessage = (TextView) findViewById(R.id.group_chat_text_display);
+        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
+    }
+}
